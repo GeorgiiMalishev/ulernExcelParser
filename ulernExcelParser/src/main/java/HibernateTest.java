d12c8ed80c1297757be538880693bbd505b4e849
GeorgiiMalishev <malishev292@gmail.com>
2023-11-26 22:52:50 +0500

    1

--- ulernExcelParser/src/main/java/HibernateTest.java
+++ ulernExcelParser/src/main/java/HibernateTest.java
@@ -1,21 +1,12 @@
-import entities.Student;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateTest {
     public static void main(String[] args) {
-        // Создание фабрики EntityManagerFactory
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("sqlitePU");
-
-        // Открытие нового EntityManager
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        // Ваш код для работы с базой данных
-
-        // Закрытие EntityManager и EntityManagerFactory
-        entityManager.close();
-        entityManagerFactory.close();
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory factory = configuration.
+                buildSessionFactory(builder.build());
     }
 }
